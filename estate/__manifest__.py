a8f4e408b14ef6b294f6778afd29b6627de539c5
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 13:24:55 +0200

    Rename model access

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-        "security/ir.estate.property.access.csv",
+        "security/ir.model.access.csv",
 
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
