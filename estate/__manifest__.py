f9e928cab2c76d0bceded902fcc50ba2733c3195
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 16:48:44 +0200

    Add view and rights for tags

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
 
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
+        "views/estate_property_tags_views.xml",
         "views/estate_menus.xml",
     ],
     "installable": True,
