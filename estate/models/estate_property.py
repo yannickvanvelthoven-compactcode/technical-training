b0d4dcf2bd5f690bce9941f7802a3d53c40e42fc
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 11:40:45 +0200

    Update estate property name definition

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate property Model"
 
     name = fields.Char(required=True)
