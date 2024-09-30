4f246ff3b1095628811b568beac5d6626af7442a
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 11:22:27 +0200

    Added act window

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,8 @@
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
+
+        "views/estate_property_views.xml",
     ],
     "installable": True,
     'license': 'LGPL-3',
