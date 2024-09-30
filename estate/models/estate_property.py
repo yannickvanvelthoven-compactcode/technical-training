2d9cbe7bdb348830d5dfd11d49f5de131bc45cdb
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 14:00:25 +0200

    Update estate property defaults

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.today() + timedelta(days=-90)).strftime('%Y-%m-%d')
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readOnly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -18,6 +19,6 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Type',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('east', 'West')],
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Type is used to define which orientation is used"
     )
