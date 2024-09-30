6a3cb700360468ff8b74ace5c6861d8935458f88
Yannick <yannick.vanvelthoven@compactcode.eu>
2024-09-30 13:19:37 +0200

    Fix access filename

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-        "security/ir.model.access.csv",
+        "security/ir.estate.property.access.csv",
 
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
